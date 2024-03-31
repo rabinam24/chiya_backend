74951b7cc61057def95e4d288ce495d83f223d70
Rabinam <rabinbhandari070@gmail.com>
2024-03-31 22:58:06 +0545

    add register controller

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,4 +1,4 @@
-class ApiResponde {
+class ApiResponse {
     constructor(statusCode, data, message="Success"){
         this.statusCode= statusCode
         this.data= data
@@ -6,3 +6,5 @@ class ApiResponde {
         this.success= statusCode < 400
     }
 }
+
+export { ApiResponse }
