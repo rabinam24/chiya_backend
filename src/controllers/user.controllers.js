d0bad93ce469e4a7dc771b9da1c6216adfdda90f
Rabinam <rabinbhandari070@gmail.com>
2024-04-02 21:55:15 +0545

    vamosh

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -4,56 +4,62 @@ import {User} from "../models/user.models.js";
 import {uploadOnCloudinary} from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
-
-const registerUser = asyncHandler( async (req,res) => {
-
+const registerUser = asyncHandler( async (req, res) => {
     // get user details from frontend
-    //validation - not empty ( checking if it is empty or not)
-    // check if user already exits: we will check this by username, email
-    //check for images, chekc for avatar
-    //upload them to cloudinary, evatar
-    //create user object -- create entry in db
-    //remove password and refresh token field from response
-    //check for user creation
-    //return res
+    // validation - not empty
+    // check if user already exists: username, email
+    // check for images, check for avatar
+    // upload them to cloudinary, avatar
+    // create user object - create entry in db
+    // remove password and refresh token field from response
+    // check for user creation
+    // return res
 
-    const {fullName, email, username, password}= req.body
-    console.log("email", email);
 
-    if(
-        [fullName,email,username,password].some((field)=>
-        field?.trim() === "")
-    ){
-        throw new ApiError(400,"All fields are required")
+    const {fullName, email, username, password } = req.body
+    //console.log("email: ", email);
+
+    if (
+        [fullName, email, username, password].some((field) => field?.trim() === "")
+    ) {
+        throw new ApiError(400, "All fields are required")
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ username }, { email }]
     })
 
-    if (existedUser){
-        throw new ApiError(409,"User with email or username already exists")
+    if (existedUser) {
+        throw new ApiError(409, "User with email or username already exists")
     }
+    // console.log(req.files);
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    //const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
-    if(!avatarLocalPath){
-        throw new ApiError(400,"Avatar file is required");
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
+    
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar file is required")
     }
 
-    const avatar = await uploadOnCloudinary(avatarLocalPath);
-    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
-    if(!avatar){
-        throw new ApiError(400,"Avatar file is required");
+    if (!avatar) {
+        throw new ApiError(400, "Avatar file is required")
     }
+   
 
-    const User = await User.create({
+    const user = await User.create({
         fullName,
-        avatar:avatar.url,
-        coverImage:coverImage?.url || "",
-        email,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        email, 
         password,
         username: username.toLowerCase()
     })
@@ -62,20 +68,30 @@ const registerUser = asyncHandler( async (req,res) => {
         "-password -refreshToken"
     )
 
-    if(!createdUser){
-        throw new ApiError(500,"Something went wrong while registering the user")
+    if (!createdUser) {
+        throw new ApiError(500, "Something went wrong while registering the user")
     }
 
     return res.status(201).json(
-        new ApiResponse(200,createdUser,"User Registered Successfully")
+        new ApiResponse(200, createdUser, "User registered Successfully")
     )
 
+} )
 
-
-
+const loginUser = asyncHandler(async (req,res) => {
+    //req body -> data
+    //username or email
+    //find the user
+    //password check
+    //access and refresh token
+    //send cookie
+    
 
 })
 
 
 
-export {registerUser}
+export {
+    registerUser,
+    loginUser
+}
